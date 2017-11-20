38455faf0dfd2905ed2a87aba3c0d87015d88201
longzekai <longzekaitelai@sina.com>
2017-11-20 21:26:03 +0800

    2017-11-20 21:25:51

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -1,3 +1,14 @@
+# coding=utf-8
+#print ('hello')
+# print ("hello # world")
 
-print ('hello')
-print ("hello # world")
+# print('''这是一段多行字符串。这是它的第一行。
+# This is the second line.
+# "What's your name?," I asked.
+# He said "Bond, James Bond."
+# ''')
+
+age = 20
+name = 'Swaroop'
+print ('{0} was {1} years old when he wrote this book'.format(name, age))
+print ('why is {0} playing with that python? '.format(name))
