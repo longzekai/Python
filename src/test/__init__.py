d8383ba5c453ebf7bff82351333afb0fe24535b6
Danny <longzekai@sina.com>
2017-11-20 22:57:17 +0800

    原始字符串

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -7,6 +7,7 @@
 # "What's your name?," I asked.
 # He said "Bond, James Bond."
 # ''')
+from idlelib.multicall import r
 
 age = 20
 name = 'Swaroop'
@@ -28,6 +29,15 @@ print('a', end=' ')
 print('b', end=' ')
 print('c')
 
-
-
+# 转义序列
+print('This is the first line\nThis is the second line')
+print("This is the first sentence. \
+This is the second sentence.")
+# 多行注释可用三对双引号
+"""
+原始字符串
+如果你需要指定一些未经过特殊处理的字符串，比如转义序列，那么你需要在字符串前增加
+r  或  R  来指定一个 原始（Raw） 字符串 。下面是一个例子：
+"""
+print(r"Newlines are indicated by \n")
 
