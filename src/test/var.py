1d43dbbd8a22aa78771dbe53ac5ea6e6a1a4411b
longzekai <longzekai@sina.com>
2017-11-21 14:29:17 +0800

    基础练习章节完成。

---
+++ src/test/var.py
@@ -0,0 +1,22 @@
+i = 5
+print(i)
+
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+# 不建议使用分号
+# a = 8; print(a);
+print()
+
+s = 'This is a string. \
+This continues the string.'
+print(s)
+
+i = 5
+# 下面将发生错误,注意行首有一个空格
+# print('Value is',i)
+print('I repeat, the value is', i)
+
