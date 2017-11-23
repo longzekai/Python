b3a17e260ba714800a63dd6af3b62483744dc02c
Danny <longzekai@sina.com>
2017-11-23 21:11:27 +0800

    字典练习。

--- src/test/day03/dataType.py
+++ src/test/day03/dataType.py
@@ -136,6 +136,40 @@ print(a - b)  # a和b的差集
 print(a | b)  # a和b的并集
 print(a & b)  # a和b的交集
 print(a ^ b)  # a和b中不同时存在的元素
+print('==========Set End============')
+
+print('==========Dictionary Start============')
+# Dictionary（字典）
+# 字典（dictionary）是Python中另一个非常有用的内置数据类型。
+# 列表是有序的对象结合，字典是无序的对象集合。两者之间的区别在于：
+# 字典当中的元素是通过键来存取的，而不是通过偏移存取。
+# 字典是一种映射类型，字典用"{ }"标识，它是一个无序的键(key) : 值(value)对集合。
+# 键(key)必须使用不可变类型。
+# 在同一个字典中，键(key)必须是唯一的。
+dict = {}
+dict['one'] = "1- 菜鸟教程"
+dict[2] = "2- 菜鸟工具"
+
+tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+print(dict['one'])  # 输出键为 'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
+
+# 构造函数 dict() 可以直接从键值对序列中构建字典
+# dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)])
+# print(dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)]))
+
+print({x: x**2 for x in (2, 4, 6)})
+# print(dict(Runoob=1, Google=2, Taobao=3))
+# 字典类型也有一些内置的函数，例如clear()、keys()、values()等。
+# 1、字典是一种映射类型，它的元素是键值对。
+# 2、字典的关键字必须为不可变类型，且不能重复。
+# 3、创建空字典使用 { }。
+print(tinydict.keys())
+print(tinydict.values())
+print(tinydict.items())
 
 
 
@@ -156,3 +190,12 @@ print(a ^ b)  # a和b中不同时存在的元素
 
 
 
+
+
+
+
+
+
+
+
+# end
