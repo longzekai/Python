47d5762e07f7b61153714b300d1e1b287e48c9cf
longzekai <longzekai@sina.com>
2017-11-23 18:20:52 +0800

    集合练习。

--- src/test/day03/dataType.py
+++ src/test/day03/dataType.py
@@ -90,3 +90,69 @@ tup = (1, 2, 3, 4, 5, 6)
 print(tup[0])
 print(tup[1:5])
 # tup[0] = 11  # 修改元组元素的操作是非法的
+
+# 虽然tuple的元素不可改变，但它可以包含可变的对象，比如list列表。
+# 构造包含 0 个或 1 个元素的元组比较特殊，所以有一些额外的语法规则：
+print('==========tuple3============')
+tup1 = ()  # 空元组
+tup2 = (20,)  # 一个元素，需要在元素后添加逗号
+print(tup1)
+print(tup2)
+# string、list和tuple都属于sequence（序列）。
+# 1、与字符串一样，元组的元素不能修改。
+# 2、元组也可以被索引和切片，方法一样。
+# 3、注意构造包含0或1个元素的元组的特殊语法规则。
+# 4、元组也可以使用+操作符进行拼接。
+print('==========tuple end============')
+
+print('==========Set Start============')
+# Set（集合）
+# 集合（set）是一个无序不重复元素的序列。
+# 基本功能是进行成员关系测试和删除重复元素。
+# 可以使用大括号 { } 或者 set() 函数创建集合，注意：创建一个空集合必须用 set() 而不是 { }，
+# 因为 { } 是用来创建一个空字典。
+# 创建格式：
+# parame = {value01,value02,...}
+# 或者
+# set(value)
+parame = {2, 3, 4, 'danny'}
+print(parame)
+print('parame is', type(parame))
+print(set())
+print(set([2, 3, 4, 5, 6, 4 + 3j]))
+
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+print(student)  # 输出集合，重复的元素被自动去掉
+# 成员测试
+if 'Rose' in student:
+    print('Rose 在集合中')
+else:
+    print('Rose 不在集合中')
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+print(a - b)  # a和b的差集
+print(a | b)  # a和b的并集
+print(a & b)  # a和b的交集
+print(a ^ b)  # a和b中不同时存在的元素
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
